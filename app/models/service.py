1c66265e2ac857bcd8f5b72fbacbd34d0990ac04
DerrickMits <derrickodiwuor@gmail.com>
2024-07-05 12:21:26 +0000

    update

--- app/models/service.py
+++ app/models/service.py
@@ -4,8 +4,13 @@ import hospital_models as models
 from hospital_models.base_model import HospitalBaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Enum
 from sqlalchemy.orm import relationship
+import enum
+
+class ServiceType(enum.Enum):
+    IN_PATIENT = "in-patient"
+    OUT_PATIENT = "out-patient"
 
 class Service(HospitalBaseModel, Base):
     """Representation of Service """
@@ -15,12 +20,14 @@ class Service(HospitalBaseModel, Base):
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         price = Column(Integer, nullable=False, default=0)
+        service_type = Column(Enum(ServiceType), nullable=False)
         facility = relationship("Facility", backref="services")
     else:
         facility_id = ""
         name = ""
         description = ""
         price = 0
+        service_type = ""
 
     def __init__(self, *args, **kwargs):
         """initializes Service"""
